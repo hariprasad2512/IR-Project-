27ee6e4e1419803a11030010e71890fd44bbba4f
Hariprasad Anuganti <111779312+hariprasad2512@users.noreply.github.com>
2023-12-10 23:28:31 +0530

    Searchbar UI

--- code/app.py
+++ code/app.py
@@ -1,7 +1,8 @@
+import time
 from flask import Flask, render_template, request
 
 # Import your existing project modules
-from web_scraping import scrape_gutenberg_india, get_book_content
+from web_scraping import scrape_gutenberg_india
 from text_processing import preprocess_text, build_inverted_index
 from searching import search, rank_search_results
 from filtering import filter_by_genre, filter_by_author
@@ -10,16 +11,18 @@ from assessment import evaluate_system_performance
 
 app = Flask(__name__)
 
-# Retrieved data for demonstration purposes
+# Dummy data for demonstration purposes
 books = scrape_gutenberg_india()
 inverted_index = build_inverted_index(books)
 
 @app.route('/')
 def index():
-    return render_template('index.html', search_results=None)
+    return render_template('index.html', search_results=None, time_taken=None)
 
 @app.route('/search', methods=['POST'])
 def search():
+    start_time = time.time()  # Record start time
+
     query = request.form['query']
 
     # Implement your search logic here
@@ -35,7 +38,7 @@ def search():
     search_results = [{'title': book, 'score': 1.0} for book in relevant_books]
     search_results.sort(key=lambda x: x['score'], reverse=True)
 
-    return render_template('index.html', search_results=search_results)
+    end_time = time.time()  # Record end time
+    time_taken = end_time - start_time
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('index.html', search_results=search_results, time_taken=time_taken)
